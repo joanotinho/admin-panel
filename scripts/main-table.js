0586a20ce7fd35df49e00d01603471acf490d22c
joanotinho <joanbaixauli2004@gmail.com>
2022-04-19 19:47:16 +0200

    transitions done

--- scripts/main-table.js
+++ scripts/main-table.js
@@ -1,12 +1,14 @@
 const editButtons = document.querySelectorAll('.edit-button');
 const deleteButtons = document.querySelectorAll('.delete-button');
-const container = document.querySelector('.container');
+const formColumn = document.querySelector('.form-column');
+const tableColumn = document.querySelector('.table-column');
 
 
 export function mainTable() {
     editButtons.forEach(editButton => {
         editButton.addEventListener('click', () => {
-            container.classList.toggle('active');
+            formColumn.classList.toggle('active');
+            tableColumn.classList.toggle('active');
         });
     });
 }
